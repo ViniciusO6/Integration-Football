5c8b4b6b0b15676a0abc6a1fc83c174785720ddb
vinium12 <125403506+vinium12@users.noreply.github.com>
2024-10-19 01:27:35 -0300

    Add ContatoNovo.php

--- Integration-Football/ContatoNovo.php
+++ Integration-Football/ContatoNovo.php
@@ -7,43 +7,74 @@
     "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
     "https://fonts.gstatic.com/",
     "https://fonts.googleapis.com/css2?family=Barlow&family=Teko:wght@300&display=swap"
-
+    
   ];
   $titulo = 'Contato';
-  $pageCSS = ["ContatoNovo.css"];
+  $pageCSS = ["ContatoNovo2.css"];
   $pageJS = ["Login.js"];
 
-  include_once('./templetes/index.php');
+  include_once('./templetes/menu.php');
 
 ?>
 
 
 
 <div class="container">
-    
-    <div class="conteudo">
-      <h1>Fale Conosco</h1>
-        <div class="opçoes">
-            <div class="contato card">
-                <img src="Imagens/Email.svg" alt="">
-                <p class="titulo">+ Contatos</p>
-                <p class="descricao">Integrationfootball.central@gmail.com</p>
-            </div>
+    <div class ="titulo"><h1>Fale Conosco</h1></div>
+        <div class="conteudo">
+            
+                    <div class="opcoes">
+                        
+                            <div class="card largura" onclick="window.location.href='https://mail.google.com/mail/?view=cm&fs=1&to=integrationfootball.central@gmail.com';">
+                                <div class="imagens">
+                                    <img src="Imagens/Mensagem.svg" alt="" draggable="false"> 
+                                </div>
 
-            <div class="matricula card">
-                <img src="Imagens/Fone.svg" alt="">
-                <p class="titulo">Central de Matrícula</p>
-                <p class="descricao">(11)99999-9999</p>
-                <p class="descricao">(11)99999-9999</p>
-            </div>
+                                <div class="texto">
+                                    <h2>+ Contatos</h2>
 
-            <div class="unidades card">
-                <img src="Imagens/Unidades.svg" alt="">
-                <p class="titulo">Mapa de Unidades</p>
-                <p class="descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                        <div class="descricao">    
+                                            <p>integrationfootball.central@gmail.com</p>
+                                        </div>
+
+                                </div>
+                            </div>
+
+                        <div class="card tamanho">
+                            <div class="imagens">
+                                <img src="Imagens/Telefone.svg" alt="" draggable="false">
+                            </div>
+                            <div class="texto">
+                                <h2>Central de Matrículas</h2>
+
+                                    <div class="descricao">   
+                                        <p>(11)98753-8346</p>
+                                        <p>(11)93244-5464</p>
+                                    </div>
+
+                            </div>
+                        </div>
+                        
+
+
+                        <div class="card largura"  onclick="redirecionar('Unidades.html')">
+                            <div class="imagens">
+                                <img src="Imagens/Mapa.svg" alt="" draggable="false">
+                            </div>
+                            <div class="texto">
+                                <h2>Mapa de Unidades</h2>
+
+                            <div class="descricao">
+                                <p>Localize a unidade mais próxima através do nosso mapa de unidades.</p>
+                            </div>
+
+                            </div>
+                        </div>
+
+
+
+                    </div>
             </div>
-        </div>
-    </div>
 
 
 </div>
