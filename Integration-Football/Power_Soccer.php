5f51c728cf0ace6976a993b0bcfde5ecd7f22f88
Karinne Angelo <125403596+Kakventura@users.noreply.github.com>
2024-10-20 20:58:55 -0300

    Atualizando projeto Power Soccer

--- Integration-Football/Power_Soccer.php
+++ Integration-Football/Power_Soccer.php
@@ -2,23 +2,20 @@
 // Os imports são usados para adicionar links de fontes e bibliotecas externas no HTML
 // No caso, estão substituindo as tradicionais tags <link rel="stylesheet">
 $imports = [
-  "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,400;0,600;1,200;1,400;1,600&display=swap", // Importando fonte "Poppins"
   "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css", // Importando ícones do Font Awesome
   "https://fonts.gstatic.com/", // Link necessário para funcionar fontes do Google
-  "https://fonts.googleapis.com/css2?family=Barlow&family=Teko:wght@300&display=swap",// Importando fontes adicionais
-  "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+  "https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
 ];
 
 // Variáveis que armazenam o título da página, os arquivos CSS e JS específicos da página
 $titulo = 'Power Soccer'; // Título da página
 
-$pageCSS = ["index.css"]; // Arquivo CSS específico da página
-$pageJS = ["index.js"]; // Arquivo JS específico da página
+$pageJS = ["menu.js"]; // Arquivo JS específico da página
 $pageCSS = ["power_soccer.css"]; // Arquivo CSS dos Cards
 
 
 // Inclui o arquivo index.php que contém a estrutura base da página
-include_once('./templetes/index.php');
+include_once('./templetes/menu.php');
 ?>
 
 <!--TOPO PRIMEIRA PARTE(FOTO E EXPLICAÇÃO)-->
