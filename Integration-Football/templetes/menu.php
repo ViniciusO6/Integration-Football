e72624924d05866b66064224696d176476a48705
Karinne Angelo <125403596+Kakventura@users.noreply.github.com>
2024-10-20 18:02:22 -0300

    Atualizando HomePage

--- Integration-Football/templetes/menu.php
+++ Integration-Football/templetes/menu.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="templetes/header.css">
+    <link rel="stylesheet" href="templetes/menu.css">
     <link rel="stylesheet" href="templetes/footer.css">
-    <link href="https://fonts.googleapis.com/css2?family=Barlow&family=Teko:wght@300&display=swap" rel="stylesheet">
+    <link rel="icon" href="./Imagens/icon.jpeg" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="templetes/menu.js" defer></script>
     <title><?= $titulo ?></title>
