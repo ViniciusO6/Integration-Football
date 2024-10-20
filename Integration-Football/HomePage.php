625f2f9bf55ad5ac25b1dbbcc9e206ddc15c5e33
Karinne Angelo <125403596+Kakventura@users.noreply.github.com>
2024-10-20 17:58:16 -0300

    Atualizando HomePage

--- Integration-Football/HomePage.php
+++ Integration-Football/HomePage.php
@@ -5,7 +5,7 @@
   $imports =[
     "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
     "https://fonts.gstatic.com/",
-    "https://fonts.googleapis.com/css2?family=Barlow&family=Teko:wght@300&display=swap"
+    "https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
   ];
   $titulo = 'HomePage';
   $pageCSS = ["home.css"];
@@ -101,7 +101,7 @@
     </div>
   </div>
 
-  <div id="bloco3" class="bloco" onclick="window.location.href='Walking_Football.php'">>
+  <div id="bloco3" class="bloco" onclick="window.location.href='Walking_Football.php'">
   <img class="image-placeholder" src="./Imagens/walking-football.jpg">
     <div class="vertical-line"></div>
     <div class="content">
