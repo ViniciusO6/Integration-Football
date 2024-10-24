3ebfb75c818a16cfc68f4db61dcbaa21ac661928
Karinne Angelo <125403596+Kakventura@users.noreply.github.com>
2024-10-24 15:11:26 -0300

    Update login-duda.php

--- Integration-Football/login-duda.php
+++ Integration-Football/login-duda.php
@@ -1,9 +1,9 @@
 <?php
-$imports = [
-    "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,400;0,600;1,200;1,400;1,600&display=swap",
+$imports =[
     "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
-    "https://fonts.googleapis.com/css2?family=Barlow&family=Teko:wght@300&display=swap"
-];
+    "https://fonts.gstatic.com/",
+    "https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+  ];
 $titulo = 'Página de login';
 $pageCSS = ["Login.css"];
 include_once('./templetes/index.php');
