ff31a0193af5c9a1de262d6976c981196a5ecda7
vinium12 <125403506+vinium12@users.noreply.github.com>
2024-10-27 21:08:11 -0300

    Update consultaprofessor

--- Integration-Football/css/consultarprofessor.js
+++ Integration-Football/css/consultarprofessor.js
@@ -1,9 +1,11 @@
 function AbrirJustificativa(element) {
   const arquivoDiv = document.getElementById("card-justificativa");
+  const arquivoInterno = document.getElementById("content-justificativa");
   const Aprov = document.getElementById("aprovado");
   const Reprov = document.getElementById("reprovado");
 
   arquivoDiv.classList.toggle("aberto");
+  arquivoInterno.classList.toggle("aberto");
   Aprov.classList.toggle("clicado");
   Reprov.classList.toggle("clicado");
   element.classList.toggle("ativo");
