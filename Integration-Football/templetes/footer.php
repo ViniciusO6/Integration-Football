1f49328e13242c2be470db75b4c2d4a69d832943
Vinicius Augusto <125403644+ViniciusO6@users.noreply.github.com>
2024-11-11 00:39:30 -0300

    Revert "Atualização da Karinne"
    This reverts commit 53c804ba411cb40b703cac400548e770759f7ded.

--- Integration-Football/templetes/footer.php
+++ Integration-Football/templetes/footer.php
@@ -1,58 +1,42 @@
-
-    <footer>
-        <div class="container-footer">
-            <div class="row-footer">
-                <!-- footer col-->
-                <div class="footer-col">
-                    <h4>SOBRE A INSTITUIÇÃO</h4>
-                    <ul>
-                        <li><a href="#"> Quem somos </a></li>
-                        <li><a href=""> Nossos Projetos e serviços </a></li>
-                        <li><a href=""> Política de Privacidade </a></li>
-                        <li><a href=""> Programa de Afiliados</a></li>
-                    </ul>
-                </div>
-                <!--end footer col-->
-                <!-- footer col-->
-                <div class="footer-col">
-                    <h4>FALE CONOSCO</h4>
-                    <ul>
-                        <li><a href="#"></a></li>
-                        <li><a href="#">(11) 94349-4679</a></li>
-                        <li><a href="#">(11) 4002-8922</a></li>
-                        <li><a href="#">integrationfuteb@gmail.com</a></li>
-                        
-                    </ul>
-                </div>
-                <!--end footer col-->
-                <!-- footer col-->
-               
-                <!--end footer col-->
-                <!-- footer col-->
-                <div class="footer-col">
-                    <h4>Se subescreva!</h4>
-                    <div class="form-sub">
-                        <form>
-                            <input type="email" placeholder="  Digite o seu e-mail" required>
-                            <button>Subscrever</button>
-                        </form>
-                    </div>
-    
-                    <div class="medias-socias">
-                        <a href="#"> <i class="fa fa-facebook"></i> </a>
-                        <a href="#"> <i class="fa fa-instagram"></i> </a>
-                        <a href="#"> <i class="fa fa-twitter"></i> </a>
-                        <a href="#"> <i class="fa fa-linkedin"></i> </a>
-                    </div>
-    
-                </div>
-                <!--end footer col-->
+<footer>
+    <div class="container-footer">
+        <div class="row-footer">
+            <!-- footer col -->
+            <div class="footer-col">
+                <h4>SOBRE A INSTITUIÇÃO</h4>
+                <ul>
+                    <li><a href="#">Quem somos</a></li>
+                    <li><a href="#">Nossos Projetos e serviços</a></li>
+                    <li><a href="#">Política de Privacidade</a></li>
+                    <li><a href="#">Programa de Afiliados</a></li>
+                </ul>
             </div>
+            <!-- end footer col -->
+            <!-- footer col -->
+            <div class="footer-col">
+                <h4>FALE CONOSCO</h4>
+                <ul>
+                    <li><a href="#">(11) 94349-4679</a></li>
+                    <li><a href="#">(11) 4002-8922</a></li>
+                    <li><a href="#">integrationfootball@gmail.com</a></li>
+                </ul>
+            </div>
+            <!-- end footer col -->
+            <!-- footer col -->
+            <div class="footer-col">
+                <h4>Se inscreva!</h4>
+                <div class="form-sub">
+                    <form>
+                    <input type="email" name="input6" id="emailInput" placeholder="Digite seu email:" required>
+<button type="submit" id="subscribeButton">Subscrever</button>
+
+                    </form>
+                </div>
+            </div>
+            <!-- end footer col -->
         </div>
-        <div class="direitos">
-          <p><strong>1° Desenvolvimento de Sistemas - AMS (ETEC ZONA LESTE). </strong> Todos os direitos reservados &copy; 2023</p>
-        </div>
-      </footer>
-    
-</body>
-</html>
+    </div>
+    <div class="direitos">
+        <p><strong>2° Desenvolvimento de Sistemas - AMS (ETEC ZONA LESTE).</strong> Todos os direitos reservados &copy; 2024</p>
+    </div>
+</footer>
