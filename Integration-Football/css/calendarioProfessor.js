e0d54670866fa57c28a23a5c1d4fb118f7beb219
vinium12 <125403506+vinium12@users.noreply.github.com>
2024-11-11 23:34:29 -0300

    Update Calendario Professor

--- Integration-Football/css/calendarioProfessor.js
+++ Integration-Football/css/calendarioProfessor.js
@@ -84,8 +84,17 @@ function abrirPrevia(IDAtividade, IDCard) {
   DivCardArquivo.classList.toggle("aberto");
 }
 
-if (file) {
-  document.getElementById("file-text").innerHTML = file.name;
-} else {
-  document.getElementById("fileName").textContent = "";
-}
+// Código do Input
+document.getElementById("input-arquivo").addEventListener("click", function () {
+  document.getElementById("files").click();
+});
+
+document.getElementById("files").addEventListener("change", function () {
+  var file = document.getElementById("files").files[0];
+
+  if (file) {
+    document.getElementById("file-text").innerHTML = file.name;
+  } else {
+    document.getElementById("fileName").textContent = "";
+  }
+});
