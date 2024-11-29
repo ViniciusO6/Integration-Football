cd10ac9e0fba1b0c1cc942c451ff0a69e243bac5
Vinicius Augusto <125403644+ViniciusO6@users.noreply.github.com>
2024-11-28 23:07:30 -0300

    Reapply "Merge branch 'main' of https://github.com/ViniciusO6/Integration-Football"
    This reverts commit 11fd69c997e8d1ef3f90427955af32c0a0534874.

--- Integration-Football/analiseInscricao.php
+++ Integration-Football/analiseInscricao.php
@@ -5,7 +5,7 @@ $imports = [
     "https://fonts.gstatic.com/",
     "https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
 ];
-$titulo = 'Visualizar Inscrições';
+$titulo = 'Detalhes da Inscrição';
 
 // Inclui o cabeçalho com base no padrão da instituição
 include_once('./templetes/headerInstituicao.php');
@@ -32,27 +32,50 @@ $row = $result->fetch_assoc();
 ?>
 
 <link rel="stylesheet" href="css/analiseInscricao.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 
 <div class="container">
-    <h1>Detalhes da Inscrição</h1>
-    <table>
-        <tr><th>ID:</th><td><?= $row['id'] ?></td></tr>
-        <tr><th>Nome:</th><td><?= $row['nome_inscrito'] ?></td></tr>
-        <tr><th>Data de Nascimento:</th><td><?= $row['data_nasc'] ?></td></tr>
-        <tr><th>CPF:</th><td><?= $row['Cpf_inscrito'] ?></td></tr>
-        <tr><th>CPF do Responsável:</th><td><?= $row['CpfResponsavel'] ?></td></tr>
-        <tr><th>RG:</th><td><?= $row['RG_inscrito'] ?></td></tr>
-        <tr><th>RG do Responsável:</th><td><?= $row['RgReponsavel'] ?></td></tr>
-        <tr><th>E-mail:</th><td><?= $row['email_inscrito'] ?></td></tr>
-        <tr><th>E-mail do Responsável:</th><td><?= $row['emailResponsavel'] ?></td></tr>
-        <tr><th>Telefone:</th><td><?= $row['telefone_inscrito'] ?></td></tr>
-        <tr><th>Telefone do Responsável:</th><td><?= $row['telResponsavel'] ?></td></tr>
-        <tr><th>Modalidade:</th><td><?= $row['modalidadeInscrito'] ?></td></tr>
-        <tr><th>Unidade:</th><td><?= $row['unidadeInscrito'] ?></td></tr>
-        <tr><th>Gênero:</th><td><?= $row['genero_inscrito'] ?></td></tr>
-        <tr><th>Deficiência:</th><td><?= $row['deficiencia'] ?></td></tr>
-    </table>
-    
+    <h1>DETALHES DA INSCRIÇÃO</h1>
+    <form>
+        <div class="form-group">
+            <label for="id">ID:</label>
+            <input type="text" id="id" value="<?= $row['id'] ?>" readonly>
+        </div>
+        <div class="form-group">
+            <label for="nome">Nome:</label>
+            <input type="text" id="nome" value="<?= $row['nome_inscrito'] ?>" readonly>
+        </div>
+        <div class="form-group">
+            <label for="data_nasc">Data de Nascimento:</label>
+            <input type="text" id="data_nasc" value="<?= $row['data_nasc'] ?>" readonly>
+        </div>
+        <div class="form-group">
+            <label for="cpf">CPF:</label>
+            <input type="text" id="cpf" value="<?= $row['Cpf_inscrito'] ?>" readonly>
+        </div>
+        <div class="form-group">
+            <label for="cpf_responsavel">CPF do Responsável:</label>
+            <input type="text" id="cpf_responsavel" value="<?= $row['CpfResponsavel'] ?>" readonly>
+        </div>
+        <div class="form-group">
+            <label for="rg">RG:</label>
+            <input type="text" id="rg" value="<?= $row['RG_inscrito'] ?>" readonly>
+        </div>
+        <div class="form-group">
+            <label for="email">E-mail:</label>
+            <input type="email" id="email" value="<?= $row['email_inscrito'] ?>" readonly>
+        </div>
+        <div class="form-group">
+            <label for="modalidade">Modalidade:</label>
+            <input type="text" id="modalidade" value="<?= $row['modalidadeInscrito'] ?>" readonly>
+        </div>
+        <!-- Campo de deficiência ajustado -->
+        <div class="form-group span-two">
+            <label for="deficiencia">Deficiência:</label>
+            <textarea id="deficiencia" readonly><?= $row['deficiencia'] ?></textarea>
+        </div>
+    </form>
     <div class="actions">
         <form action="aceitarInscricao.php" method="POST" style="display:inline;">
             <input type="hidden" name="id" value="<?= $row['id'] ?>">
@@ -65,4 +88,3 @@ $row = $result->fetch_assoc();
     </div>
 </div>
 
-<?php include_once('./templetes/footer.php'); ?>
